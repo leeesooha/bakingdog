8d40411b4c94609e695ec8745b2bc5f4904c670c
leeesooha <suha630@naver.com>
2024-08-16 00:19:50 +0900

    add 15651

--- BackTracking/15651/main.cpp
+++ BackTracking/15651/main.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+
+// bool isused[10];
+int arr[10];
+int N, M;
+
+void solve(int k) {
+    if (k == M) {
+        for (int i = 0; i < M; i++)
+            std::cout << arr[i] << " ";
+        std::cout << "\n";
+        return ;
+    }
+    for (int i = 1; i <= N; i++) {
+        // isused[i] = true; //바킹독의 백트래킹의 전형적형태이지만 현 문제에선 필요없음.
+        arr[k] = i;
+        solve(k + 1);
+        // isused[i] = false;
+    }
+}
+
+int main(void) {
+    std::cin >> N >> M;
+
+    solve(0);
+    return 0;
+}
