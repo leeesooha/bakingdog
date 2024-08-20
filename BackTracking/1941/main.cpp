02805660f5949ce69adae3a3bf66a3b6498c3a44
leeesooha <suha630@naver.com>
2024-08-20 23:52:36 +0900

    add 1941

--- BackTracking/1941/main.cpp
+++ BackTracking/1941/main.cpp
@@ -1,61 +1,55 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <string>
+#include <algorithm>
+#include <tuple>
 
-int num[25];
-int arr[10];
+int ans;
+int mask[25];
 std::string board[5];
-std::vector<std::vector<int>> AllCases;
-
-int cnt;
-
-void findAllCases(int k, int st) {
-    if (k == 7) {
-        std::vector<int> a(arr, arr + 7);
-        AllCases.push_back(a);
-        for (int j = 0; j < 7; j++)
-            std::cout << a[j];
-        std::cout << std::endl;
-        return ;
-    }
-    
-    for (int i = st; i < 25; i++) {
-        if (k != 0) {
-            if (i <= arr[k - 1]) continue;
-            int diff = i - arr[k - 1];
-            if (!(diff == 1 || diff == 5))
-                continue;
-        }
-    
-        if (i + 1 < 25) {
-            std::cout << "right" << std::endl;
-            if (i % 5 == 0)
-                continue;
-            arr[k] = i;
-            findAllCases(k + 1, i + 1);
-        }
-        if (i + 5 < 25) {
-            std::cout << "down" << std::endl;
-            arr[k] = i;
-            findAllCases(k + 1, i + 5);
-        }
-    }
-}
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 
 int main(void) {
-    for (int i = 0; i < 25; i++)
-        num[i] = i;
-
     for (int i = 0; i < 5; i++)
-        std::cin >> board[i];
+        std::cin >> board[i]; 
 
-    findAllCases(0, 0);
-    // for (int i = 0; i < 10; i++) {
-    //     for (int j = 0; j < 7; j++) {
-    //         std::cout << AllCases[i][j];
-    //     }
-    //     std::cout << std::endl;
-    // }
+    std::fill(mask + 7, mask + 25, true);
+    do {
+        std::queue<std::pair<int, int> > Q;
+        bool vis[5][5] = {};
+        bool isp[5][5] = {};
+        for (int i = 0; i < 25; i++) {
+            if (!mask[i]) {
+                int x = i / 5;
+                int y = i % 5;
+                isp[x][y] = true;
+                if (Q.empty()) {
+                    Q.push({x, y});
+                    vis[x][y] = true;
+                }
+            }
+        }
+        int adj = 0;
+        int dasom = 0;
+        while (!Q.empty()) {
+            int cur_x, cur_y;
+            std::tie(cur_x, cur_y) = Q.front(); Q.pop();
+            adj++;
+            if (board[cur_x][cur_y] == 'S')
+                dasom++;
+            for (int i = 0 ; i < 4; i++) {
+                int nx = cur_x + dx[i];
+                int ny = cur_y + dy[i];
+                if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5 || vis[nx][ny] || !isp[nx][ny]) continue;
+                Q.push({nx, ny});
+                vis[nx][ny] = true;
+            }
+        }
+        if (adj >= 7 && dasom >= 4)
+            ans++;
+    } while (std::next_permutation(mask, mask + 25));
+    std::cout << ans;
+
     return 0;
 }
-
