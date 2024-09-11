50af7a10a2a881db0ae156105e88270ad1c2130e
leeesooha <suha630@naver.com>
2024-09-11 23:17:29 +0900

    solve 18808

--- simulation/18808/main.cpp
+++ simulation/18808/main.cpp
@@ -10,28 +10,18 @@ void rotate_sticker() {
     int temp_int;
 
     //스티커 복사
-    for (int i = 0; i < 12; i++) {
-        for (int j = 0; j < 12; j++) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
             temp[i][j] = sticker[i][j];
         }
     }
 
-    //원본스티커 초기화
-    for (int i = 0; i < 12; i++) {
-        for (int j = 0; j < 12; j++) {
-            std::fill(sticker[i], sticker[i] + 12, -1);
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            sticker[i][j] = temp[r - 1 - j][i];
         }
     }
-
-    //스티커 회전 적용
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
-            sticker[i][j] = temp[j][r - 1 - j];
-        }
-    }
-    temp_int = r;
-    r = c;
-    c = temp_int;
+    std::swap(r, c);
 }
 
 //모눈 종이 그리기 함수
@@ -55,9 +45,8 @@ std::pair<int, int> where_stick() {
         for (int j = 0; j < m; j++) {
 
             isValid = true;
-            int sr, sc;
-            for (sr = 0; sr < r && isValid; sr++) {
-                for (sc = 0; sc < c && isValid; sc++) {
+            for (int sr = 0; sr < r && isValid; sr++) {
+                for (int sc = 0; sc < c && isValid; sc++) {
                     if (i + sr >= n || j + sc >= m) {
                         isValid = false;
                         break;
@@ -70,8 +59,6 @@ std::pair<int, int> where_stick() {
                 if (isValid == false)
                     break ;
             }
-            if (!(sr == r && sc == c))
-                isValid = false;
             if (isValid == false)
                 continue;
             pos = {i, j};
@@ -88,46 +75,25 @@ int main(void) {
 
     for (int i = 0; i < k; i++) { //각 모눈종이 모두 확인반복
         std::cin >> r >> c;
-        for (int j = 0; j < 10; j++)
-            std::fill(sticker[j], sticker[j] + 10, -1);
         for (int j = 0; j < r; j++) {   //모눈 종이 하나 입력 받기
             for (int l = 0; l < c; l++) {
                 std::cin >> sticker[j][l];
             }
         }
 
-        // std::cout << "----0" << "\n";  
         // 노트북에 모눈 종이 4방향 다 넣어보기
         for (int j = 0; j < 4; j++) {
             //노트북에 모눈 종이 어디에 들어가는지 확인 == true or false
             std::pair<int, int> pos = where_stick();
-            // std::cout << "----1" << "\n";
-            std::cout << "[" << pos.first << "," << pos.second << "]" << "\n";
             if (!(pos.first == -1)) { //if 양수
                 stick_sticker(pos.first, pos.second);   // 그 자리에 삽입
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < m; j++) {
-                        std::cout << board[i][j] << " ";
-                    }
-                    std::cout << "\n";
-                }
-                std::cout << "\n";
                 break ;
             }
             //모눈 종이 회전
             rotate_sticker();
-            std::cout << "\n\nrotate: \n";
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
-                    std::cout << sticker[i][j] << " ";
-                }
-                std::cout << "\n";
-            }
-            std::cout << "\n";
         }
 
     }
-    // std::cout << "----10" << "\n";
     //노트북에 1 적힌거 개수 셈.
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
